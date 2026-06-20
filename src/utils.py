a697c58411567aee5e5f5cacfc524ff11826f616
jiacomas <jcomas.fr@gmail.com>
2026-06-20 21:06:42 +0200

    create results 02

--- src/utils.py
+++ src/utils.py
@@ -16,6 +16,7 @@ PROCESSED_DL_DIR = f"{OUTPUT_DIR}/dl"
 
 OUTPUT_DIR_EDA = f"{PROJECT_ROOT}/figures/eda"
 MODELS_DIR = f"{PROJECT_ROOT}/models"
+RESULTS_DIR = f"{PROJECT_ROOT}/result"
 
 # --- Configuració general ---
 RANDOM_SEED = 42
